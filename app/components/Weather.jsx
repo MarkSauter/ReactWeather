ebfdbf2bae8e0302432750bc88e921515bef9c73
Mark Sauter <Mark Sauter>
2016-10-08 17:36:20 -0500

    Initial commit

---
+++ app/components/Weather.jsx
@@ -0,0 +1,67 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+var WeatherForm = require('WeatherForm');
+var WeatherMessage = require('WeatherMessage');
+var openWeatherMap = require('openWeatherMap');
+
+class Weather extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      isLoading: false
+    }
+
+    // This explicit binding is needed without babel stage-0 plugin
+    //  this.handleSearch = this.handleSearch.bind(this);
+    // OR one can use react-autobind
+    //  Example:
+    //      import autoBind from 'react-autobind';
+    //      constructor() {
+    //        super();
+    //        autobind(this); // automatic binding with smart avoidance of React related methods
+    //          OR
+    //        autobind(this, 'myMehod1, 'myMethod2'); // explicit binding
+    //      }
+
+  }
+
+  // This function declaration is considered experimental ES7.
+  // It only works with babel stage-0 plugin
+  handleSearch = (location) => {
+    this.setState({ isLoading: true });
+    openWeatherMap.getTemp(location).then(temp => {
+
+      this.setState({
+        location: location,
+        temp: temp,
+        isLoading: false
+      });
+    }, errorMessage => {
+      this.setState({ isLoading: false });
+      alert(errorMessage);
+    });
+  }
+
+  render () {
+    var {isLoading, location, temp} = this.state;
+
+    function renderMessage () {
+      if (isLoading) {
+        return <h3>Fetching weather...</h3>;
+      } else if (temp && location) {
+        return <WeatherMessage location={location} temp={temp}/>;
+      }
+    }
+
+    return (
+      <div>
+        <h2>Get Weather</h2>
+        <WeatherForm onSearch={this.handleSearch}/>
+        {renderMessage()}
+      </div>
+    );
+  }
+}
+
+module.exports = Weather;
---
+++ app/components/WeatherForm.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var WeatherForm = React.createClass({
+
+  onFormSubmit: function (e) {
+    e.preventDefault();
+
+    var location = this.refs.location.value.trim();
+
+    if (location) {
+      this.refs.location.value = '';
+      this.props.onSearch(location);
+    }
+  },
+
+  render: function() {
+    return (
+      <div>
+        <form onSubmit={this.onFormSubmit}>
+          <input type="text" ref="location" placeholder="Enter city name"/>
+          <button>Get Weather</button>
+        </form>
+      </div>
+    );
+  }
+
+});
+
+module.exports = WeatherForm;
