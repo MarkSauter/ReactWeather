4bcfd797988e1cc9f03ee739e809bd07dc4b9ece
Mark Sauter <Mark Sauter>
2016-10-10 22:18:25 -0500

    Switch to scss

--- app/app.jsx
+++ app/app.jsx
@@ -11,7 +11,7 @@ require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
 
 // App css
-require('style!css!appplicationStyle')
+require('style!css!sass!appplicationStyle')
 
 ReactDOM.render(
   <Router history={hashHistory}>
--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
       Examples: 'app/components/Examples.jsx',
       openWeatherMap: 'app/api/openWeatherMap.jsx',
       ErrorModal: 'app/components/ErrorModal.jsx',
-      appplicationStyle: 'app/styles/app.css'
+      appplicationStyle: 'app/styles/app.scss'
     },
     extensions: ['', '.js', '.jsx']
   },
